af24c9bc31ed539fe18e54ead73d052fbeb435cf
Георги Тинчев <georgi.tinchev.124@gmail.com>
2025-05-29 10:07:19 +0300

    Removed leetcode
    Will use the actual leetcode site to avoid unnecessary tracking

--- Leetcode/xpi_2s.py
+++
@@ -1,8 +0,0 @@
-def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # for index in length of input arr
-        for i in range(len(nums)):
-            # next element in range from 1 to end-remaining
-            for j in range(i + 1, len(nums)):
-                # if current + next equals target return indices of elements
-                if nums[i] + nums[j] == target:
-                    return [i,j]
